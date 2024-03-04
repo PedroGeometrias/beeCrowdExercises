475bb2bf6b8cc26eb5c8951d8c58ee192f65faf6
Pedro <portasemasaneta@gmail.com>
2024-03-04 01:21:19 -0300

    more c

---
+++ cSubmissions/1145_Submission.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    for (int i = 0, count = 1; count <= y; ++i) {
+        for (int j = 1; j <= x && count <= y; ++j, ++count) {
+            if (j > 1) {
+                printf(" ");
+            }
+            printf("%d", count);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
