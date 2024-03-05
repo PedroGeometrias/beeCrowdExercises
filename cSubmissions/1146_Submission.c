73ccc6cf2dd3a95f3c5a643252f37c4c891d249e
Pedro <portasemasaneta@gmail.com>
2024-03-05 03:12:03 -0300

    more programming

---
+++ cSubmissions/1146_Submission.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int x;
+    while(scanf("%d", &x), x !=0 ){
+        for (int i = 1; i <= x; ++i) {
+            printf("%d", i);
+            if(i != x){
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ cSubmissions/1149_Submission.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int a, int n) {
+    int sum = 0;
+    for (int i = a; i < a + n; ++i) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main() {
+    int n, a;
+    scanf("%d %d", &a, &n);
+    while (n <= 0) {
+        scanf("%d", &n);
+    }
+    printf("%d\n", sum(a, n));
+    return 0;
+}
